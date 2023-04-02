d55a5aed1f1326e3122ffc9897cfcbdd4758f3a8
Fazıl Demir <fazildemir20@gmail.com>
2023-04-02 03:30:05 +0300

    kahve makinesi

---
+++ src/main/java/kahvemakinesi/Runner.java
@@ -0,0 +1,20 @@
+package kahvemakinesi;
+
+
+import static kahvemakinesi.Methods.menuu;
+
+public class Runner extends Depo {
+
+    public static void main(String[] args) {
+
+
+        menuu();
+
+
+    }
+
+
+
+
+
+}
